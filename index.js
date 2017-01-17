d520e6ab20c41aa7cd932a8b32767348dc5a7386
Sushruth Shastry <sushruth@users.noreply.github.com>
2017-01-17 12:47:13 -0500

    JSON stub creation
    Thanks to contributors

--- index.js
+++ index.js
@@ -2,65 +2,57 @@
 // @name        CVS stub
 // @namespace   cvs.xhr.interceptor
 // @description XHR stub creator
-// @include     https://www-qa1.cvs.com/pharmacy/
+// @include     https://www-qa1.cvs.com/pharmacy/*
 // @version     1
 // @grant       none
+// @require     https://stuk.github.io/jszip/vendor/FileSaver.js
+// @require     https://unpkg.com/jszip@latest/dist/jszip.js
+// @run-at      document-start
 // ==/UserScript==
-
 'use strict';
-
 var COMPLETED_READY_STATE = 4;
-
 var RealXHRSend = XMLHttpRequest.prototype.send;
-
 var requestCallbacks = [];
 var responseCallbacks = [];
-
 var wired = false;
 
-function arrayRemove(array,item) {
+function arrayRemove(array, item) {
     var index = array.indexOf(item);
     if (index > -1) {
         array.splice(index, 1);
     } else {
-        throw new Error("Could not remove " + item + " from array");
+        throw new Error('Could not remove ' + item + ' from array');
     }
 }
 
-
-function fireCallbacks(callbacks,xhr) {
-    for( var i = 0; i < callbacks.length; i++ ) {
+function fireCallbacks(callbacks, xhr) {
+    for (var i = 0; i < callbacks.length; i++) {
         callbacks[i](xhr);
     }
 }
-
 
 var addRequestCallback = function(callback) {
     requestCallbacks.push(callback);
 };
 var removeRequestCallback = function(callback) {
-    arrayRemove(requestCallbacks,callback);
+    arrayRemove(requestCallbacks, callback);
 };
-
-
 var addResponseCallback = function(callback) {
     responseCallbacks.push(callback);
 };
 var removeResponseCallback = function(callback) {
-    arrayRemove(responseCallbacks,callback);
+    arrayRemove(responseCallbacks, callback);
 };
 
-
-
 function fireResponseCallbacksIfCompleted(xhr) {
-    if( xhr.readyState === COMPLETED_READY_STATE ) {
-        fireCallbacks(responseCallbacks,xhr);
+    if (xhr.readyState === COMPLETED_READY_STATE) {
+        fireCallbacks(responseCallbacks, xhr);
     }
 }
 
 function proxifyOnReadyStateChange(xhr) {
     var realOnReadyStateChange = xhr.onreadystatechange;
-    if ( realOnReadyStateChange ) {
+    if (realOnReadyStateChange) {
         xhr.onreadystatechange = function() {
             fireResponseCallbacksIfCompleted(xhr);
             realOnReadyStateChange();
@@ -68,49 +60,53 @@ function proxifyOnReadyStateChange(xhr) {
     }
 }
 
-
 var isWired = function() {
     return wired;
 }
 
 var wire = function() {
-    if ( wired ) throw new Error("Ajax interceptor already wired");
-
+    if (wired) throw new Error('Ajax interceptor already wired');
     // Override send method of all XHR requests
     XMLHttpRequest.prototype.send = function() {
-
         // Fire request callbacks before sending the request
-        fireCallbacks(requestCallbacks,this);
-
+        fireCallbacks(requestCallbacks, this);
         // Wire response callbacks
-        if( this.addEventListener ) {
+        if (this.addEventListener) {
             var self = this;
-            this.addEventListener("readystatechange", function() {
+            this.addEventListener('readystatechange', function() {
                 fireResponseCallbacksIfCompleted(self);
             }, false);
-        }
-        else {
+        } else {
             proxifyOnReadyStateChange(this);
         }
-
         RealXHRSend.apply(this, arguments);
     };
     wired = true;
 };
 
-
 var unwire = function() {
-    if ( !wired ) throw new Error("Ajax interceptor not currently wired");
+    if (!wired) throw new Error('Ajax interceptor not currently wired');
     XMLHttpRequest.prototype.send = RealXHRSend;
     wired = false;
 };
+var zip = new JSZip();
 
 wire();
 
-addRequestCallback(function(xhr) {
-    console.debug("request",xhr);
+addResponseCallback(function(xhr) {
+    if (!xhr.resource.url.match(/(\.js$|.html$)/g)) {
+        var requestName = xhr.resource.url.split('?')[0].split('/').slice(-1)[0] + '.json';
+        console.debug('request', requestName);
+        console.debug('response', xhr.response);
+        zip.file(requestName, xhr.response);
+    }
 });
 
-.addResponseCallback(function(xhr) {
-    console.debug("response",xhr);
-});
+unsafeWindow.downloadStubZip = function() {
+    zip.generateAsync({
+        type: 'blob'
+    }).then(function(content) {
+        // see FileSaver.js
+        saveAs(content, 'example.zip');
+    });
+}
